8a61eaa57f5fa878136700d56ce7610c5c23c102
Tharindu N <dc.chathu9@gmail.com>
2023-05-09 20:54:08 +0530

    updated homepage header
    Co-Authored-By: GayathmaHiruni <131847817+GayathmaHiruni@users.noreply.github.com>
    Co-Authored-By: MethmiDharmakeerthi <97521363+MethmiDharmakeerthi@users.noreply.github.com>
    Co-Authored-By: Prasadini30 <125174664+Prasadini30@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import Index from "./components/index";
-import Home from "./components/home";
-import Login from "./components/login";
-import Reservations from "./components/reservations";
-import BusManagment from "./components/busManagment";
-import Contact from "./components/contact";
-import About from "./components/about";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Index />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/reservations" element={<Reservations />} />
-				<Route path="/managment" element={<BusManagment />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/about" element={<About />} />
 			</Routes>
 		</div>
 	);
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,38 +1,39 @@
 import React from "react";
-import styled from "styled-components";
 
-const IndexStyle = styled.div`
-	h1 {
-		color: #000;
-	}
-`;
+import "./styles/index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBus } from "@fortawesome/free-solid-svg-icons";
 
 const Index = () => {
 	return (
 		<React.Fragment>
-			<IndexStyle>
-				<h1>Index</h1>
-				<ul>
-					<li>
-						<a href="/home">Home</a>
-					</li>
-					<li>
-						<a href="/login">Login</a>
-					</li>
-					<li>
-						<a href="/reservations">Bus Reservations</a>
-					</li>
-					<li>
-						<a href="/managment">Bus Managment</a>
-					</li>
-					<li>
-						<a href="/contact">Contact Us</a>
-					</li>
-					<li>
-						<a href="/about">About</a>
-					</li>
-				</ul>
-			</IndexStyle>
+			<div className="home">
+				<div className="home-header">
+					<div className="home-header-image-container">
+						<img
+							src="homepage.png"
+							alt="homepage"
+							className="home-header-image"
+						/>
+					</div>
+
+					<div className="home-header-container">
+						<div className="home-header-logo">
+							<FontAwesomeIcon icon={faBus} />
+						</div>
+						<div className="home-header-title">
+							Welcome to Guider
+						</div>
+						<div className="home-header-subtitle">
+							Online Advance Bus Seat Reservation
+						</div>
+					</div>
+				</div>
+
+				<div className="home-body">
+					<h1>Body</h1>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };
