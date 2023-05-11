2efd9404e65217b509caea581c81a0ab6fbdb22f
Tharindu N <dc.chathu9@gmail.com>
2023-05-11 10:06:04 +0530

    add a table to reservations

--- src/pages/reservations.jsx
+++ src/pages/reservations.jsx
@@ -64,61 +64,107 @@ const Reservations = () => {
 				</div>
 
 				<div className="reservations-body">
-					<form className="reservations-bus-form">
-						<div className="reservations-form-group">
-							<label htmlFor="reservations-bus-number">
-								Select Bus Number:
-							</label>
-							<select
-								id="reservations-bus-number"
-								name="bus-number"
-								required
-								className="reservations-input"
-								onChange={handleBusNumberChange}
-							>
-								<option value="">Select bus number</option>
-								{busses.map((bus) => (
-									<option value={bus.id} key={bus.id}>
-										{bus.busNumber}
-									</option>
-								))}
-							</select>
-						</div>
-						<div className="reservations-form-group">
-							<label htmlFor="reservations-passengers-count">
-								Passengers Count:
-							</label>
-							<input
-								type="number"
-								id="reservations-passengers-count"
-								name="passengers-count"
-								min="1"
-								max="100"
-								required
-								className="reservations-input reservations-input-passengers"
-							/>
-						</div>
-						<div className="reservations-form-group">
-							<label htmlFor="reservations-time-slot">
-								Time Slot:
-							</label>
-							<select
-								id="reservations-time-slot"
-								name="time-slot"
-								required
-								className="reservations-input"
-							>
-								{showingTimeSlots.map((timeSlot) => (
-									<option value={timeSlot} key={timeSlot}>
-										{timeSlot}
-									</option>
-								))}
-							</select>
-						</div>
-						<button type="submit" className="reservations-btn">
-							Take a ticket
-						</button>
-					</form>
+					<div className="reservations-bus-form-container">
+						<form className="reservations-bus-form">
+							<div className="reservations-form-group">
+								<label htmlFor="reservations-bus-number">
+									Select Bus Number:
+								</label>
+								<select
+									id="reservations-bus-number"
+									name="bus-number"
+									required
+									className="reservations-input"
+									onChange={handleBusNumberChange}
+								>
+									<option value="">Select bus number</option>
+									{busses.map((bus) => (
+										<option value={bus.id} key={bus.id}>
+											{bus.busNumber}
+										</option>
+									))}
+								</select>
+							</div>
+							<div className="reservations-form-group">
+								<label htmlFor="reservations-passengers-count">
+									Passengers Count:
+								</label>
+								<input
+									type="number"
+									id="reservations-passengers-count"
+									name="passengers-count"
+									min="1"
+									max="100"
+									required
+									className="reservations-input reservations-input-passengers"
+								/>
+							</div>
+							<div className="reservations-form-group">
+								<label htmlFor="reservations-time-slot">
+									Time Slot:
+								</label>
+								<select
+									id="reservations-time-slot"
+									name="time-slot"
+									required
+									className="reservations-input"
+								>
+									{showingTimeSlots.map((timeSlot) => (
+										<option value={timeSlot} key={timeSlot}>
+											{timeSlot}
+										</option>
+									))}
+								</select>
+							</div>
+							<button type="submit" className="reservations-btn">
+								Take a ticket
+							</button>
+						</form>
+					</div>
+
+					<div className="reservations-table-container">
+						<table class="reservations-table">
+							<thead>
+								<tr>
+									<th class="reservations-table_header">
+										No.
+									</th>
+									<th class="reservations-table_header">
+										But Number
+									</th>
+									<th class="reservations-table_header">
+										Passengers
+									</th>
+									<th class="reservations-table_header">
+										Time
+									</th>
+									<th class="reservations-table_header">
+										Actions
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="reservations-table_data">1</td>
+									<td class="reservations-table_data">
+										#B001
+									</td>
+									<td class="reservations-table_data">2</td>
+									<td class="reservations-table_data">
+										10:00 AM
+									</td>
+									<td class="reservations-table_data">
+										<button class="reservations-table_btn">
+											Edit
+										</button>
+										<button class="reservations-table_btn">
+											Delete
+										</button>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</React.Fragment>
