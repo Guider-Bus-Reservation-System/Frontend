202e469d6d45d2037a79ea8b627b073abf01ca58
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-07-07 01:25:47 +0530

    modify contact page

--- src/components/contact/ContactPage.jsx
+++ src/components/contact/ContactPage.jsx
@@ -3,41 +3,88 @@ import "./styles/ContactPage.css";
 
 const ContactPage = () => {
 	return (
-		<div className="contact-container">
-			<h2>Contact Us</h2>
-			<form className="contact-form">
-				<div className="row">
-					<div className="col-25">
-						<label for="name">Name : </label>
+		<React.Fragment>
+			<div className="contact-container">
+				<div className="contact-container-left">
+					<div className="contact-container-left-header">
+						<h2>Get in touch</h2>
 					</div>
-					<div className="col-75">
-						<input type="text" placeholder="Your Name" />
-					</div>
-				</div>
+					<form className="contact-form">
+						<div className="row">
+							<div className="col-25">
+								<label for="name">Name : </label>
+							</div>
+							<div className="col-75">
+								<input type="text" placeholder="Your Name" />
+							</div>
+						</div>
 
-				<div className="row">
-					<div className="col-25">
-						<label for="email">Email : </label>
-					</div>
-					<div className="col-75">
-						<input type="email" placeholder="Your Email" />
-					</div>
-				</div>
+						<div className="row">
+							<div className="col-25">
+								<label for="email">Email : </label>
+							</div>
+							<div className="col-75">
+								<input
+									type="email"
+									rows="10"
+									placeholder="Your Email"
+								/>
+							</div>
+						</div>
 
-				<div className="row">
-					<div className="col-25">
-						<label for="message">Message : </label>
-					</div>
-					<div className="col-75">
-						<textarea placeholder="Your Message"></textarea>
-					</div>
-				</div>
+						<div className="row">
+							<div className="col-25">
+								<label for="message">Message: </label>
+							</div>
+							<div className="col-75">
+								<textarea placeholder="Your Message"></textarea>
+							</div>
+						</div>
 
-				<div className="row">
-					<button type="submit">Send Message</button>
+						<div className="row">
+							<button type="submit">Send Message</button>
+						</div>
+					</form>
 				</div>
-			</form>
-		</div>
+				<div className="contact-container-right">
+					<div className="contact-container-right-header">
+						<h2>Contact us</h2>
+					</div>
+					<ul class="contact-list">
+						<li class="list-item">
+							<i class="fa fa-map-marker fa-2x">
+								<span class="contact-text place">
+									No 123, Colombo 7, Sri Lanka
+								</span>
+							</i>
+						</li>
+
+						<li class="list-item">
+							<i class="fa fa-phone fa-2x">
+								<span class="contact-text phone">
+									<a
+										href="tel:1-212-555-5555"
+										title="Give me a call"
+									>
+										(+94) 555-2368
+									</a>
+								</span>
+							</i>
+						</li>
+
+						<li class="list-item">
+							<i class="fa fa-envelope fa-2x">
+								<span class="contact-text gmail">
+									<a href="mailto:#" title="Send me an email">
+										guider@gmail.com
+									</a>
+								</span>
+							</i>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</React.Fragment>
 	);
 };
 
