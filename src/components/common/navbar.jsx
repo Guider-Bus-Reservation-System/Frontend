28051110fa93cf72764f00846770848ca5834629
MethmiDharmakeerthi <97521363+MethmiDharmakeerthi@users.noreply.github.com>
2023-08-07 23:20:12 +0530

    Correct Nav Bar
    1. Correct nav bar item order
    2. Changed the Nav bar font face font weight styles transition CSS

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -8,28 +8,34 @@ const Navbar = () => {
 		<React.Fragment>
 			<div className="navbar">
 				<div className="navbar-links">
-					<div className="navbar-links-item">
-						<Link to="/" className="active">
-							Home
-						</Link>
+					 {/*navigation bar item 6*/}
+					 <div className="navbar-links-item">
+						<Link to="/contact">Contact Us</Link>
 					</div>
+					
+					{/*navigation bar item 5*/}
+					<div className="navbar-links-item">
+						<Link to="/about">About</Link>				
+					</div>
+					
+					{/*navigation bar item 4*/}
 					<div className="navbar-links-item">
 						<Link to="/timetable">Timetable</Link>
 					</div>
-					<div className="navbar-links-item">
-						<Link to="/reservations">Reservations</Link>
-					</div>
+					{/*navigation bar item 3*/}
 					<div className="navbar-links-item">
 						<Link to="/news">News</Link>
 					</div>
+					{/*navigation bar item 2*/}
 					<div className="navbar-links-item">
-						<Link to="/contact">Contact Us</Link>
+						<Link to="/reservations">Reservations</Link>
 					</div>
-					<div
-						className="navbar-links-item"
+					
+					{/*navigation bar item 1*/}
+					<div className="navbar-links-item"
 						style={{ float: "left" }}
 					>
-						<Link to="/about">About</Link>
+						<Link to="/" className="active">Home</Link>
 					</div>
 				</div>
 			</div>
