061ad5b9f57f1c7b78809b115328151cdf4f4581
Prasadini30 <125174664+Prasadini30@users.noreply.github.com>
2023-08-09 23:01:24 +0530

    Add timetable data

--- src/pages/timetable.jsx
+++ src/pages/timetable.jsx
@@ -51,43 +51,105 @@ const Timetable = () => {
 					<table className="timetable-table">
 						<thead>
 							<tr>
-								<th className="timetable-table_header">No.</th>
+								<th className="timetable-table_header">ID</th>
+								<th className="timetable-table_header">Bus Number</th>
 								<th className="timetable-table_header">From</th>
 								<th className="timetable-table_header">To</th>
-								<th className="timetable-table_header">
-									Distance
-								</th>
-								<th className="timetable-table_header">
-									Depart Time
-								</th>
-								<th className="timetable-table_header">
-									Arrival Time
-								</th>
-								<th className="timetable-table_header">
-									Price
-								</th>
+								<th className="timetable-table_header">Depeture Time</th>
+								<th className="timetable-table_header">Arrival Time </th>
+								<th className="timetable-table_header">Contact Number</th>
+								<th className="timetable-table_header">Price</th>
+								<th className="timetable-table_header">Availability</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td className="timetable-table_data">1</td>
-								<td className="timetable-table_data">
-									New York
-								</td>
-								<td className="timetable-table_data">
-									Los Angeles
-								</td>
-								<td className="timetable-table_data">
-									2,475 miles
-								</td>
-								<td className="timetable-table_data">
-									10:00 AM
-								</td>
-								<td className="timetable-table_data">
-									5:00 PM
-								</td>
-								<td className="timetable-table_data">$300</td>
+								<td className="timetable-table_data">NC3746 </td>
+								<td className="timetable-table_data">Kadawatha </td>
+								<td className="timetable-table_data">Mathara </td>
+								<td className="timetable-table_data">6:00 AM</td>
+								<td className="timetable-table_data">9:00 AM</td>
+								<td className="timetable-table_data">0719065263</td>
+								<td className="timetable-table_data">Rs:800</td>
+								<td className="timetable-table_data" style={{ color: 'green' }}>yes</td>
+
 							</tr>
+
+							<tr>
+								<td className="timetable-table_data">2</td>
+								<td className="timetable-table_data">ND8050 </td>
+								<td className="timetable-table_data">Kadawatha </td>
+								<td className="timetable-table_data">Makubura </td>
+								<td className="timetable-table_data">6:20 AM</td>
+								<td className="timetable-table_data">6:20 AM</td>
+								<td className="timetable-table_data">0774103485</td>
+								<td className="timetable-table_data">Rs:300</td>
+								<td className="timetable-table_data" style={{ color: 'red' }}>No</td>
+
+							</tr>
+                            
+							<tr>
+								<td className="timetable-table_data">3</td>
+								<td className="timetable-table_data">NC4324 </td>
+								<td className="timetable-table_data">Kadawatha </td>
+								<td className="timetable-table_data">Mathara </td>
+								<td className="timetable-table_data">6:45 AM</td>
+								<td className="timetable-table_data">9:30 AM</td>
+								<td className="timetable-table_data">0719065263</td>
+								<td className="timetable-table_data">Rs:800</td>
+								<td className="timetable-table_data" style={{ color: 'green' }}>yes</td>
+							</tr>
+
+							<tr>
+								<td className="timetable-table_data">4</td>
+								<td className="timetable-table_data">CD5069 </td>
+								<td className="timetable-table_data">Kadawatha </td>
+								<td className="timetable-table_data">Galle </td>
+								<td className="timetable-table_data">6:00 AM</td>
+								<td className="timetable-table_data">8:30 AM</td>
+								<td className="timetable-table_data">0726916104</td>
+								<td className="timetable-table_data">Rs:700</td>
+								<td className="timetable-table_data" style={{ color: 'green' }}>yes</td>
+							</tr>
+
+							<tr>
+								<td className="timetable-table_data">5</td>
+								<td className="timetable-table_data">NC7896 </td>
+								<td className="timetable-table_data">Mathara </td>
+								<td className="timetable-table_data"> Galle</td>
+								<td className="timetable-table_data">10:00 AM</td>
+								<td className="timetable-table_data">13:00 PM</td>
+								<td className="timetable-table_data">0724476303</td>
+								<td className="timetable-table_data">Rs:600</td>
+								<td className="timetable-table_data" style={{ color: 'green' }}>yes</td>
+							</tr>
+
+							<tr>
+								<td className="timetable-table_data">6</td>
+								<td className="timetable-table_data">NC7890 </td>
+								<td className="timetable-table_data">Mathara </td>
+								<td className="timetable-table_data">Negambo </td>
+								<td className="timetable-table_data">11:00 AM</td>
+								<td className="timetable-table_data">14:00 PM</td>
+								<td className="timetable-table_data">0765165978</td>
+								<td className="timetable-table_data">Rs:1000</td>
+								<td className="timetable-table_data" style={{ color: 'red' }}>No</td>
+
+							</tr>
+
+							<tr>
+								<td className="timetable-table_data">7</td>
+								<td className="timetable-table_data">CD3490 </td>
+								<td className="timetable-table_data">Meerigama </td>
+								<td className="timetable-table_data">Kurunegala </td>
+								<td className="timetable-table_data">15:00 AM</td>
+								<td className="timetable-table_data">17:30 AM</td>
+								<td className="timetable-table_data">0785674536</td>
+								<td className="timetable-table_data">Rs:800</td>
+								<td className="timetable-table_data" style={{ color: 'green' }}>yes</td>
+							</tr>
+						
 						</tbody>
 					</table>
 				</div>
