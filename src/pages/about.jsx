e757216815481baa518325a9c0a75ce55c78bf33
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-08-09 22:37:13 +0530

    Add about 2nd version

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,75 +6,85 @@ import Gallery from "./gallery";
 import "./styles/about.css";
 
 const About = () => {
-	return (
-		<React.Fragment>
-			<div className="about">
-				<div className="about-header">
-					<div className="about-header-navbar">
-						<Navbar />
-					</div>
+  return (
+    <React.Fragment>
+      <div className="about">
+        <div className="about-header">
+          <div className="about-header-navbar">
+            <Navbar />
+          </div>
 
-					<div className="about-header-image-container">
-						<img
-							src="homepage.png"
-							alt="homepage"
-							className="about-header-image"
-						/>
-					</div>
+          <div className="about-header-image-container">
+            <img src="homepage.png" alt="homepage" className="about-header-image" />
+          </div>
 
-					<div className="home-header-container">
-						<div className="home-header-title">About</div>
-						<div className="home-header-subtitle">
-							Guider: Expressway Bus Transport Management &
-							Information System
-						</div>
-					</div>
-				</div>
-				<div className="about-content">
-                    <div className="welcome">
-                        <h2><bold>Welcome to Guider</bold></h2><br/>
-                        <p>Your comprehensive Expressway Bus Transport Management & Information System in Sri Lanka. Our mission is to revolutionize the way people travel by providing efficient and reliable bus transportation services. With Guider, we aim to make commuting a seamless and enjoyable experience for all passengers.</p>
-                    </div>
-
-					<div className="about-content">
-					<div className="content-with-image">
-						<img src="your-vision-image.jpg" alt="Our Vision" className="content-image" />
-						<div className="content-text">
-							<h2>Our Vision</h2>
-							<p>At Guider, we envision a future where public transportation is accessible, convenient, and sustainable. We strive to create a reliable and efficient bus transport system that connects people, communities, and businesses across Sri Lanka. Our goal is to contribute to the development of a greener and more interconnected society.</p>
-						</div>
-					</div>
-
-
-					<div className="content-with-image">
-        <div className="content-text">
-            <h2>What We Offer</h2>
-            <ul>
-                <li>Bus Scheduling and Routing:</li>
-                {/* ... Rest of the content ... */}
-            </ul>
+          <div className="home-header-container">
+            <div className="home-header-title">About</div>
+            <div className="home-header-subtitle">
+              Guider: Expressway Bus Transport Management & Information System
+            </div>
+          </div>
         </div>
-        <img src="your-offer-image.jpg" alt="What We Offer" className="content-image" />
-    </div>
-</div>
-						<div className="get-in-touch">
-                        <h2>Get in Touch</h2>
-                        <p>We believe in open communication and value your thoughts and suggestions. If you have any questions, feedback, or partnership inquiries, please don't hesitate to reach out to our team. We're here to assist you and make your experience with Guider exceptional. Thank you for choosing Guider as your preferred Expressway Bus Transport Management & Information System. We look forward to serving you and making your daily commute a smooth and enjoyable journey.</p>
-                    </div>
-                </div>
-					
-				
-				<div className="Gallery">
-					<Gallery />
-				</div>
-				<div className="about-footer">
-					<Footer />
-				</div>
+        <div className="about-content">
+          <div className="welcome">
+            <h2>Welcome to Guider</h2>
+            <br />
+            <p>
+              Your comprehensive Expressway Bus Transport Management & Information System in Sri
+              Lanka. Our mission is to revolutionize the way people travel by providing efficient
+              and reliable bus transportation services. With Guider, we aim to make commuting a
+              seamless and enjoyable experience for all passengers.
+            </p>
+          </div>
 
+          <div className="about-content">
+            <div className="content-with-image">
+              <img src="your-vision-image.jpg" alt="Our Vision" className="content-image" />
+              <div className="content-text">
+                <h2>Our Vision</h2>
+                <p>
+                  At Guider, we envision a future where public transportation is accessible,
+                  convenient, and sustainable. We strive to create a reliable and efficient bus
+                  transport system that connects people, communities, and businesses across Sri
+                  Lanka. Our goal is to contribute to the development of a greener and more
+                  interconnected society.
+                </p>
+              </div>
+            </div>
 
-			</div>
-		</React.Fragment>
-	);
+            <div className="content-with-image">
+              <div className="content-text">
+                <h2>What We Offer</h2>
+                <ul>
+                  <li>Bus Scheduling and Routing:</li>
+                  {/* ... Rest of the content ... */}
+                </ul>
+              </div>
+              <img src="your-offer-image.jpg" alt="What We Offer" className="content-image" />
+            </div>
+          </div>
+          <div className="get-in-touch">
+            <h2>Get in Touch</h2>
+            <p>
+              We believe in open communication and value your thoughts and suggestions. If you have
+              any questions, feedback, or partnership inquiries, please don't hesitate to reach out
+              to our team. We're here to assist you and make your experience with Guider
+              exceptional. Thank you for choosing Guider as your preferred Expressway Bus Transport
+              Management & Information System. We look forward to serving you and making your daily
+              commute a smooth and enjoyable journey.
+            </p>
+          </div>
+        </div>
+
+        <div className="Gallery">
+          <Gallery />
+        </div>
+        <div className="about-footer">
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default About;
--- src/pages/reservations.jsx
+++ src/pages/reservations.jsx
@@ -6,133 +6,118 @@ import Footer from "../components/common/footer";
 import "./styles/reservations.css";
 
 const Reservations = () => {
-	const [showingTimeSlots, setShowingTimeSlots] = useState([]);
+  const [showingTimeSlots, setShowingTimeSlots] = useState([]);
 
-	const busses = [
-		{
-			id: 1,
-			busNumber: "#B001",
-			availableTimeSlots: ["7.00AM", "10.00AM", "4.00PM"],
-		},
-		{
-			id: 2,
-			busNumber: "#B002",
-			availableTimeSlots: ["8.00AM", "11.00AM", "5.00PM"],
-		},
-		{
-			id: 3,
-			busNumber: "#B003",
-			availableTimeSlots: ["9.00AM", "12.00AM", "6.00PM"],
-		},
-	];
+  const busses = [
+    {
+      id: 1,
+      busNumber: "#B001",
+      availableTimeSlots: ["7.00AM", "10.00AM", "4.00PM"],
+    },
+    {
+      id: 2,
+      busNumber: "#B002",
+      availableTimeSlots: ["8.00AM", "11.00AM", "5.00PM"],
+    },
+    {
+      id: 3,
+      busNumber: "#B003",
+      availableTimeSlots: ["9.00AM", "12.00AM", "6.00PM"],
+    },
+  ];
 
-	function handleBusNumberChange(event) {
-		if (event.target.value === "") {
-			setShowingTimeSlots([]);
-			return;
-		}
+  function handleBusNumberChange(event) {
+    if (event.target.value === "") {
+      setShowingTimeSlots([]);
+      return;
+    }
 
-		const selectedBus = busses.find(
-			(bus) => bus.id === parseInt(event.target.value)
-		);
+    const selectedBus = busses.find((bus) => bus.id === parseInt(event.target.value));
 
-		setShowingTimeSlots(selectedBus.availableTimeSlots);
-	}
+    setShowingTimeSlots(selectedBus.availableTimeSlots);
+  }
 
-	return (
-		<React.Fragment>
-			<div className="reservations">
-				<div className="reservations-header">
-					<div className="reservations-header-navbar">
-						<Navbar />
-					</div>
+  return (
+    <React.Fragment>
+      <div className="reservations">
+        <div className="reservations-header">
+          <div className="reservations-header-navbar">
+            <Navbar />
+          </div>
 
-					<div className="reservations-header-image-container">
-						<img
-							src="homepage.png"
-							alt="homepage"
-							className="reservations-header-image"
-						/>
-					</div>
+          <div className="reservations-header-image-container">
+            <img src="homepage.png" alt="homepage" className="reservations-header-image" />
+          </div>
 
-					<div className="home-header-container">
-						<div className="home-header-title">Reservations</div>
-						<div className="home-header-subtitle">
-							Guider: Expressway Bus Transport Management &
-							Information System
-						</div>
-					</div>
-				</div>
+          <div className="home-header-container">
+            <div className="home-header-title">Reservations</div>
+            <div className="home-header-subtitle">
+              Guider: Expressway Bus Transport Management & Information System
+            </div>
+          </div>
+        </div>
 
-				<div className="reservations-body">
-					<div className="reservations-bus-form-container">
-						<form className="reservations-bus-form">
-							<div className="reservations-form-group">
-								<label htmlFor="reservations-bus-number">
-									Select Bus Number:
-								</label>
-								<select
-									id="reservations-bus-number"
-									name="bus-number"
-									required
-									className="reservations-input"
-									onChange={handleBusNumberChange}
-								>
-									<option value="">Select bus number</option>
-									{busses.map((bus) => (
-										<option value={bus.id} key={bus.id}>
-											{bus.busNumber}
-										</option>
-									))}
-								</select>
-							</div>
-							<div className="reservations-form-group">
-								<label htmlFor="reservations-passengers-count">
-									Passengers Count:
-								</label>
-								<input
-									type="number"
-									id="reservations-passengers-count"
-									name="passengers-count"
-									min="1"
-									max="100"
-									required
-									className="reservations-input reservations-input-passengers"
-								/>
-							</div>
-							<div className="reservations-form-group">
-								<label htmlFor="reservations-time-slot">
-									Time Slot:
-								</label>
-								<select
-									id="reservations-time-slot"
-									name="time-slot"
-									required
-									className="reservations-input"
-								>
-									{showingTimeSlots.map((timeSlot) => (
-										<option value={timeSlot} key={timeSlot}>
-											{timeSlot}
-										</option>
-									))}
-								</select>
-							</div>
-							<button type="submit" className="reservations-btn">
-								Take a ticket
-							</button>
-						</form>
-						<div className="reservations-calender-container"></div>
-					</div>
+        <div className="reservations-body">
+          <div className="reservations-bus-form-container">
+            <form className="reservations-bus-form">
+              <div className="reservations-form-group">
+                <label htmlFor="reservations-bus-number">Select Bus Number:</label>
+                <select
+                  id="reservations-bus-number"
+                  name="bus-number"
+                  required
+                  className="reservations-input"
+                  onChange={handleBusNumberChange}
+                >
+                  <option value="">Select bus number</option>
+                  {busses.map((bus) => (
+                    <option value={bus.id} key={bus.id}>
+                      {bus.busNumber}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="reservations-form-group">
+                <label htmlFor="reservations-passengers-count">Passengers Count:</label>
+                <input
+                  type="number"
+                  id="reservations-passengers-count"
+                  name="passengers-count"
+                  min="1"
+                  max="100"
+                  required
+                  className="reservations-input reservations-input-passengers"
+                />
+              </div>
+              <div className="reservations-form-group">
+                <label htmlFor="reservations-time-slot">Time Slot:</label>
+                <select
+                  id="reservations-time-slot"
+                  name="time-slot"
+                  required
+                  className="reservations-input"
+                >
+                  {showingTimeSlots.map((timeSlot) => (
+                    <option value={timeSlot} key={timeSlot}>
+                      {timeSlot}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <button type="submit" className="reservations-btn">
+                Take a ticket
+              </button>
+            </form>
+            <div className="reservations-calender-container"></div>
+          </div>
+        </div>
 
-
-				</div>
-
-				<div className="reservations-footer">
-					<Footer />
-				</div>
-			</div>
-		</React.Fragment>
-	);
+        <div className="reservations-footer">
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Reservations;
